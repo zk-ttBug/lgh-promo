00bbe0b34a17dc47f1f610ff92056bd26642bb66
rahul <ro87630872@126.com>
2016-06-27 20:13:23 +0800

    【UPD】添加主题标签

---
+++ components/wap_theme_tag/data.js
@@ -0,0 +1,63 @@
+module.exports = {
+    bottom:false,                    //true|false    是否显示下划线
+    background:true,                //true|false    是否显示背景色
+    links:[
+        {
+
+            tags:[
+                {
+                    href:"#",
+                    color:"red",  //red
+                    title:"佳利雅"
+                },
+                {
+                    href:"#",
+                    color:"red",  //red
+                    title:"佳利雅"
+                },
+                {
+                    href:"#",
+                    color:"red",
+                    title:"瑞记"
+                },
+                {
+                    href:"#",
+                    color:"red",
+                    title:"牛栏"
+                },
+                {
+                    href:"#",
+                    title:"达能"
+                }
+            ]
+        },
+        {
+
+            tags:[
+                {
+                    href:"#",
+                    title:"摇铃"
+                },
+                {
+                    href:"#",
+                    color:"red",  //red
+                    title:"佳利雅"
+                },
+                {
+                    href:"#",
+                    color:"red",
+                    title:"学步"
+                },
+                {
+                    href:"#",
+                    color:"red",
+                    title:"游泳"
+                },
+                {
+                    href:"#",
+                    title:"爬行垫"
+                }
+            ]
+        }
+    ]
+};
