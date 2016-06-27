3f6219192232fc1f02e4a088e9bbb997a126a97d
rahul <ro87630872@126.com>
2016-06-27 20:04:36 +0800

    【UPD】添加主题板式组件

--- components/wap_2col_img/data1.js
+++ components/wap_2col_img/data1.js
@@ -1,10 +1,10 @@
 module.exports = {
 	img1: {
 		imgUrl: "http://7xvh3g.com1.z0.glb.clouddn.com/timg.jpg", // 左边图片路径
-		href: "http://www.baidu.com" // 左边图片超链接
+		href: "http://www.baidu.com", // 左边图片超链接,
 	},
 	img2: {
 		imgUrl: "http://7xvh3g.com1.z0.glb.clouddn.com/timg.jpg", // 右边图片路径
-		href: "http://www.baidu.com" // 右边图片超链接
+		href: "http://www.baidu.com", // 右边图片超链接
 	}
 };
---
+++ components/wap_theme_title/data.js
@@ -0,0 +1,4 @@
+module.exports = {
+    title:'女人街',
+    color:'pink'   //green|gray|blue|cyan|pink|orange
+};
