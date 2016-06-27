094ae01edd8ac9838645ef3d3145e549794be7b6
wurh <wurh@users.noreply.github.com>
2016-06-27 23:49:31 +0800

    [UPD] 更新banner 组件

---
+++ components/wap_slider_banner/data.js
@@ -0,0 +1,14 @@
+module.exports = {
+    list: [
+        {
+            img: 'http://o9fs1vs45.bkt.clouddn.com/banner1.jpg',
+            url: 'http://www.baidu.com'
+        }, {
+            img: 'http://o9fs1vs45.bkt.clouddn.com/banner2.jpg',
+            url: 'http://www.baidu.com'
+        }, {
+            img: 'http://o9fs1vs45.bkt.clouddn.com/banner3.jpg',
+            url: 'http://www.baidu.com'
+        }
+    ]
+};
---
+++ views/m-fun1/m-fun1.js
@@ -0,0 +1,5 @@
+define(function(require){
+    var slider = require('components/wap_slider_banner/wap_slider_banner.js');
+    slider();
+    console.log('m-fun.js');
+});
