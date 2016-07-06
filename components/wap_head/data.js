d2a2bada46d0abbc265329ebdbed2d4386e7cd11
rahul <ro87630872@126.com>
2016-07-06 16:26:18 +0800

    【UPD】添加时间标题组件

--- components/wap_head/data.js
+++ components/wap_head/data.js
@@ -1,6 +1,6 @@
 module.exports = {
     content: "老广州的回忆", // 内容
     text_align: "center",
-    bgcolor:"pink",
+    bgcolor:"black",
     fontcolor:"white"
 };
---
+++ components/wap_timeline/data.js
@@ -0,0 +1,6 @@
+module.exports = {
+    tag: '原创',
+    source: '老广汇',
+    url:'http://www.laoguanghui.com/#!/index',
+    timestamp:'2016-07-06'
+};
